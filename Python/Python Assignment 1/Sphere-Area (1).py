4ae9a182d8a017ab3acf271b508d5bb5bb7ab1c9
akagrawal1-git <68939665+akagrawal1-git@users.noreply.github.com>
2020-07-31 06:57:57 +0530

    Area of Sphere

---
+++ Python/Python Assignment 1/Sphere-Area (1).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#area of a sphere 4/3 *pi*r3
+import math 
+
+radius = 6
+Area = (4/3) * math.pi * math.pow(radius,3) 
+
+print(Area)
+
+
+# In[ ]:
+
+
+
+
